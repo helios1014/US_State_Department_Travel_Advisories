d2e6a9c749a39c963c074dfd54ac2fb307743c65
Wesley James Young <109540069+helios1014@users.noreply.github.com>
2025-12-01 11:21:14 -0500

    Update feed_test.py
    added functionality to produce static map

--- feed_test.py
+++ feed_test.py
@@ -8,6 +8,8 @@ import io
 import requests
 from bs4 import BeautifulSoup
 import geopandas as gpd
+import zipfile
+import matplotlib.pyplot as plt
 # %%
 #dictionary to convert US State Department Country Codes to ISO2 as needed. 
 #in an earlier version of this RSS feed, BT was the term used for US State Department Country codes. This is retaind here as these codes still have to be recoded to ISOA2
@@ -79,56 +81,63 @@ def rating(line):
     else:
         level = 'unkown error'
     return(level)
- 
+today = pd.Timestamp.today() 
 # %%
-feed = rss_to_json(r'https://travel.state.gov/_res/rss/TAsTWs.xml')
-df =pd.read_json(io.StringIO(feed))
-df['pubDate'] = pd.to_datetime(df.pubDate)
-df.loc[df['pubDate'] > pd.Timestamp.today(), ['pubDate']] = pd.Timestamp.today() #sometimes erronious publication dates are provided
-#Hong Kong and Macau are not given country codes in the RSS feed so have to be handled seperatly. 
-df.loc[df['Name'] == 'Macau', ['ISO_A2']] = 'MO'
-df.loc[df['Name'] == 'Hong Kong', ['ISO_A2']] = 'HK'
-#the next four lines are to handle the US State Department aggregating the French overseas territories under one designation
-FT = df.loc[df.ISO_A2=='A3'].squeeze()
-FT_df = pd.DataFrame({'pubDate':[FT.iloc[1]]*4, 'ISO_A2':['GP', 'MQ', 'MF', 'BL'], 'Threat-Level':[FT.iloc[3]]*4})
-df = pd.concat([df, FT_df], ignore_index=True)
-df.drop(df[df['ISO_A2'] == 'A3'].index, inplace=True)
-df.drop('Name', axis=1, inplace=True)
+# due to loading issues with the rss feed, a try and except block was nessesary
+for attempt in range(5):
+    try:
+        feed = rss_to_json(r'https://travel.state.gov/_res/rss/TAsTWs.xml')
+        df =pd.read_json(io.StringIO(feed))
+        df['pubDate'] = pd.to_datetime(df.pubDate)
+        df.loc[df['pubDate'] > today, ['pubDate']] = today #sometimes erronious publication dates are provided
+        #Hong Kong and Macau are not given country codes in the RSS feed so have to be handled seperatly. 
+        df.loc[df['Name'] == 'Macau', ['ISO_A2']] = 'MO'
+        df.loc[df['Name'] == 'Hong Kong', ['ISO_A2']] = 'HK'
+        #the next four lines are to handle the US State Department aggregating the French overseas territories under one designation
+        FT = df.loc[df.ISO_A2=='A3'].squeeze()
+        FT_df = pd.DataFrame({'pubDate':[FT.iloc[1]]*4, 'ISO_A2':['GP', 'MQ', 'MF', 'BL'], 'Threat-Level':[FT.iloc[3]]*4})
+        df = pd.concat([df, FT_df], ignore_index=True)
+        df.drop(df[df['ISO_A2'] == 'A3'].index, inplace=True)
+        df.drop('Name', axis=1, inplace=True)
 
-if 'IL' in df['ISO_A2'].values:
-    new_date = df.loc[df['ISO_A2']=='IS', 'pubDate']
-    url = 'https://travel.state.gov/content/travel/en/traveladvisories/traveladvisories/israel-west-bank-and-gaza-travel-advisory.html'
-    response =requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    spoon = soup.find('div',class_='tsg-rwd-emergency-alert-text')
-    IL_r = rating(spoon.find_all('b', string= lambda t: t and 'Israel –' in t)[0].get_text())
-    PS_r1 = rating(spoon.find_all('b', string= lambda t: t and 'West Bank –' in t)[0].get_text())
-    PS_r2 = rating(spoon.find_all('b', string= lambda t: t and 'Gaza –' in t)[0].get_text())
-    PS_rf = {
-        1: 'level 1: exercise normal precautions', 
-        2: 'Level 2: Exercise Increased Caution', 
-        3: 'Level 3: Reconsider Travel', 
-        4: 'Level 4: Do Not Travel'
-        }.get(
-        max(
-            int(re.search(r'\d+', PS_r1)),
-            int(re.search(r'\d+', PS_r2))
-        ),
-        ''
-    )
-    PS_df = pd.DataFrame(
-        {
-            'pubDate':new_date, 
-            'ISO_A2':'PS', 
-            'Threat-Level': PS_rf
-        }
-    )
-    df = pd.concat([df, PS_df], ignore_index=True)
-    df.loc[df['ISO_A2']=='IL', 'Threat-Level'] = IL_r
-else:
-    df
-
-  
+        if 'IL' in df['ISO_A2'].values:
+            new_date = df.loc[df['ISO_A2']=='IS', 'pubDate']
+            url = 'https://travel.state.gov/content/travel/en/traveladvisories/traveladvisories/israel-west-bank-and-gaza-travel-advisory.html'
+            response =requests.get(url)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            spoon = soup.find('div',class_='tsg-rwd-emergency-alert-text')
+            IL_r = rating(spoon.find_all('b', string= lambda t: t and 'Israel –' in t)[0].get_text())
+            PS_r1 = rating(spoon.find_all('b', string= lambda t: t and 'West Bank –' in t)[0].get_text())
+            PS_r2 = rating(spoon.find_all('b', string= lambda t: t and 'Gaza –' in t)[0].get_text())
+            PS_rf = {
+                1: 'level 1: exercise normal precautions', 
+                2: 'Level 2: Exercise Increased Caution', 
+                3: 'Level 3: Reconsider Travel', 
+                4: 'Level 4: Do Not Travel'
+                }.get(
+                max(
+                    int(re.search(r'\d+', PS_r1)),
+                    int(re.search(r'\d+', PS_r2))
+                ),
+                ''
+            )
+            PS_df = pd.DataFrame(
+                {
+                    'pubDate':new_date, 
+                    'ISO_A2':'PS', 
+                    'Threat-Level': PS_rf
+                }
+            )
+            df = pd.concat([df, PS_df], ignore_index=True)
+            df.loc[df['ISO_A2']=='IL', 'Threat-Level'] = IL_r
+        else:
+            df
+        break
+    except Exception:
+        if attempt < 4:
+            time.sleep(5)
+        else:
+            raise
 # %%
 old_data = pd.read_csv(r'USSD_TAS.csv')
 old_data['pubDate'] = pd.to_datetime(old_data.pubDate)
@@ -139,18 +148,38 @@ filtered_data.drop('pubDate_y', axis=1, inplace=True)
 filtered_data.rename(columns={'pubDate_x': 'pubDate'}, inplace=True)
 # %%
 
-pd.concat([filtered_data, old_data], ignore_index=True).to_csv('USSD_TAS.csv', index=False, )
+current_data = pd.concat([filtered_data, old_data], ignore_index=True)
+current_data.to_csv('USSD_TAS.csv', index=False, )
+latest_df = current_data.sort_values(by=['ISO_A2', 'pubDate']).groupby('ISO_A2').last()
 
 # %%
-#url = "https://naciscdn.org/naturalearth/110m/cultural/ne_110m_admin_0_countries.zip"
-#world = gpd.read_file(url)
-#world.to_crs('+proj=wintri', inplace=True)
+url = "https://naciscdn.org/naturalearth/110m/cultural/ne_110m_admin_0_countries.zip"
+world = gpd.read_file(url)
+world.to_crs('+proj=wintri', inplace=True)
+latest_gdf = world.merge(latest_df, left_on='ISO_A2_EH', right_on='ISO_A2', how='left') #due to natural world having diffrent standards than ISO alpha 2, the ISO_A2_EH column is recomended for this merger
 
 # %%
-#[ i for i in world.columns.to_list() if 'A2' in i]
+today_t = today.strftime(r'%A, %B %d, %Y')
 # %%
-#ax = world.plot(column='ADMIN', edgecolor='black')
-#ax.set_axis_off()
-#ax.set_title(f'World Map of US State Department Travel Advisories for{today}', font=14, fontweight='bold')
+ax = latest_gdf.plot(
+    column='Threat-Level', 
+    edgecolor='black', 
+    legend=True,
+    cmap = 'viridis',
+    missing_kwds= {
+        'color': 'lightgrey',
+        'hatch': '///',
+        'label': 'No Data'
+    },
+    legend_kwds= {
+        'loc': 'center left', 
+        'bbox_to_anchor': (1, 0.5)
+        }
+    )
+ax.set_axis_off()
+ax.set_title(f'World Map of US State Department Travel Advisories for {today_t}', fontsize=14, fontweight='bold')
+
+plt.savefig('travel_map.svg', bbox_inches='tight', dpi=600)
 # %%
 #https://geopandas.org/en/stable/docs/user_guide/interactive_mapping.html
+
